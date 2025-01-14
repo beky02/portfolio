6c830d5d7acec47a1d43dfa1bdb1e9d29d2c0e45
Bereket <berekettadege09@gmail.com>
2025-01-14 17:10:56 +0300

    Add asset imports for images in index.ts

---
+++ assets/index.ts
@@ -0,0 +1,21 @@
+import DotImage from "./Dots.png";
+import DotFullScreenImage from "./Dots_full_screen.png";
+import DiscoverImage from "./Discover.png";
+import SherpaImage1 from "./sherpa_doc1.jpg";
+import SherpaImage2 from "./sherpa_doc2.jpg";
+import AggregateImage1 from "./aggregate1.jpg";
+import AggregateImage2 from "./aggregate2.jpg";
+import Koalo from "./koalo.png";
+import Koalo1 from "./koalo1.png";
+
+export {
+  DotImage,
+  DotFullScreenImage,
+  DiscoverImage,
+  SherpaImage1,
+  SherpaImage2,
+  AggregateImage1,
+  AggregateImage2,
+  Koalo,
+  Koalo1,
+};
